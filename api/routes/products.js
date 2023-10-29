2abfeeca55a271c9d5c4d66cf567413b4cff5dd0
Hydra Coder <96688421+ngodat213@users.noreply.github.com>
2023-10-29 23:58:13 +0700

    install MongoDB

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,7 +6,14 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    res.status(200).json({message: 'Handling POST requests to /products'});
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    }
+    res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createProduct: product
+    });
 });
 
 router.get('/:productId', (req, res, next) => {
