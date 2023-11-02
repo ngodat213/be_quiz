ce544b37196e18f6cc74e38d360d85d71ac628ba
Hydra Coder <96688421+ngodat213@users.noreply.github.com>
2023-11-02 21:41:02 +0700

    Mongoose Validation

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dbUrl = 'mongodb+srv://hydra:Code26102003@cluster0.d0dwiwa.mongodb.net/PodcastFlutter?retryWrites=true&w=majority';
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect(dbUrl);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
